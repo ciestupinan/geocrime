e7344fd550eec70c716db81682ce4e53a4b9b8e5
Cristina Estupinan <cristinaestupinan@Cristinas-MBP.localdomain>
2018-11-12 14:26:57 -0800

    cleaned up code. created static folder for js files.

---
+++ static/js/map.js
@@ -0,0 +1,98 @@
+"use strict"
+
+// Initialize map
+function initMap() {
+  const sfCoords = {lat: 37.7749, lng: -122.4194};
+  
+  // array of Marker objects
+  const markers = [];
+
+  // Create map
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: sfCoords,
+    zoom: 12
+  });
+
+  // Get crime data from API
+  $.ajax({
+    url: "https://data.sfgov.org/resource/nwbb-fxkq.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "Y6mTFPFpnPQzYXxLv0LVidpom"
+    }
+  }).done(function(data) {
+
+    const crimeData = extractRelevantData(data);
+    
+    for (let i = 0; i < crimeData.length; i++) {
+      const col = crimeData[i];
+
+      const lat = col['latitude']
+      const lng = col['longitude']
+
+      const marker = addMarker(lat, lng, map);
+      addInfoWindow(marker, col, map);
+      markers.push(marker);
+    }
+  });
+}
+
+// Return relevant data at given marker (passed in through data)
+function extractRelevantData(data){
+
+  const relevantData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const col = data[i];
+
+    relevantData.push({
+      'incident_datetime': col['incident_datetime'],
+      'incident_category': col['incident_category'],
+      'incident_subcategory': col['incident_subcategory'],
+      'incident_description': col['incident_description'],
+      'resolution': col['resolution'],
+      'latitude': parseFloat(col['latitude']),
+      'longitude': parseFloat(col['longitude'])
+    });
+  }
+  return relevantData;
+}
+
+
+// Create marker at lat long
+function addMarker(latitude, longitude, map) {
+  
+  const marker = new google.maps.Marker({
+    position: {lat: latitude, lng: longitude},
+    map: map,
+    title: `${latitude}, ${longitude}`   
+  }); 
+
+  return marker;
+}
+
+
+// Create tooltip for marker
+function addInfoWindow(marker, data, map){
+  const contentString = `<div id="content">
+    <div id="siteNotice"></div>
+    <p id="firstHeading" class="firstHeading"><b> ${data['incident_datetime']} </b></p>
+    <div id="bodyContent">
+    <p> category: ${data['incident_category']}
+    <br> subcategory: ${data['incident_subcategory']}
+    <br> description: ${data['incident_description']}
+    <br> resolution: ${data['resolution']} </p>
+    </div>
+    </div>`;
+  
+  // Add content to tooltip
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  // On click, show tooltip
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
