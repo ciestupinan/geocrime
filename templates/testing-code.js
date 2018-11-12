314cde60df2f1fe8d754d8b530293538d40eebef
Cristina Estupinan <cristinaestupinan@Cristinas-MBP.localdomain>
2018-11-12 13:36:17 -0800

    cleaned up functions and made more readbale'

--- templates/testing-code.js
+++ templates/testing-code.js
@@ -1,65 +1,74 @@
-
-
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      // Focus on San Francisco
-      center: new google.maps.LatLng(37.7749,-122.4194)
-    });
+  const sfCoords = {lat: 37.7749, lng: -122.4194};
+  const markers = [];
 
-    // Load GeoJson
-    // map.data.loadGeoJson('https://data.sfgov.org/resource/nwbb-fxkq.geojson');
+  // Create map
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: sfCoords,
+    zoom: 12
+  });
 
-    $.ajax({
-      url: "https://data.sfgov.org/resource/nwbb-fxkq.json",
-      type: "GET",
-      data: {
-        "$limit" : 5000,
-        "$$app_token" : "Y6mTFPFpnPQzYXxLv0LVidpom"
-      }
-    }).done(function(data) {
-    	for (var i = 0; i < data.length; i++) {
-        	createMarker(data[i], map);
-    	}
+  // Get crime data
+  $.ajax({
+    url: "https://data.sfgov.org/resource/nwbb-fxkq.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "Y6mTFPFpnPQzYXxLv0LVidpom"
     }
+  }).done(function(data) {
+    let crimeData = data;
+    for (let i = 0; i < crimeData.length; i++) {
+      markerData = getMarkerData(crimeData[i])
+      lat = markerData['latitude']
+      lng = markerData['longitude']
+
+      markers.push(addMarker(lat, lng, map));
+    }
+
+  });
+}
+
+function getMarkerData(data){
+  markerData = {};
+
+  markerData['incident_datetime'] = data['incident_datetime'];
+  markerData['incident_category'] = data['incident_category'];
+  markerData['incident_subcategory'] = data['incident_subcategory'];
+  markerData['incident_description'] = data['incident_description'];
+  markerData['resolution'] = data['resolution'];
+  markerData['latitude'] = parseFloat(data['latitude']);
+  markerData['longitude'] = parseFloat(data['longitude']);
+  
+  return markerData;
 }
 
 
+// Create marker at lat long
+function addMarker(latitude, longitude, map) {
+  
+  const marker = new google.maps.Marker({
+    position: {lat: latitude, lng: longitude},
+    map: map,
+    title: `${latitude}, ${longitude}`   
+  }); 
 
-
-function createMarker(dataCol, map) {
-	incident_datetime = dataCol['incident_datetime'];
-    incident_category = dataCol['incident_category'];
-    incident_subcategory = dataCol['incident_subcategory'];
-    incident_description = dataCol['incident_description'];
-    resolution = dataCol['resolution'];
-    latitude = parseFloat(dataCol['latitude']);
-    longitude = parseFloat(dataCol['longitude']);
-
-    var marker = new google.maps.Marker({       
-        position: {lat: latitude, lng: longitude}, 
-        map: map,
-        title: t      
-    }); 
-
-    var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h2 id="firstHeading" class="firstHeading">'+incident_datetime+'</h2>'+
-      '<div id="bodyContent">'+
-      '<p> category: ' + incident_category +
-      '<br> subcategory: ' + incident_subcategory +
-      '<br> description: ' + incident_description +
-      '<br> resolution: ' + resolution + '</p>' +
-      '</div>'+
-      '</div>';
-    
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-    
-    marker.addListener('click', function() {
-	    infowindow.open(map, marker);
-	});
+  return marker;
 }
+
+
+// Create tooltip for marker
+function addInfoWindow(marker, map){
+  
+  
+  // Add content to tooltip
+  let infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  // On click, show tooltip
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+  
