bfae0e7250bc1a9ddef91157146992a59a7d2dd7
Cristina Estupinan <cristinaestupinan@Cristinas-MBP.localdomain>
2018-12-14 10:47:51 -0800

    turn off debugger mode

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def index():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
 
